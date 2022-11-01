91c22c5c0a534550632b68db890312e9b796c070
stigibalint <72031994+stigibalint@users.noreply.github.com>
2022-11-01 19:41:45 +0100

    Update java.js

--- java.js
+++ java.js
@@ -7,9 +7,6 @@ function blurFunction(id) {
 }
 
 function submit(){
-    console.log(document.getElementById('mNeve').innerHTML)
-    console.log(document.getElementById('mCim').innerHTML)
-    console.log(document.getElementById('phone').innerHTML)
     document.getElementById('visszaigazol_nev').innerHTML = document.getElementById('mNeve').value;
     document.getElementById('visszaigazol_cim').innerHTML = document.getElementById('mCim').value;
     document.getElementById('visszaigazol_tel').innerHTML = document.getElementById('phone').value;
